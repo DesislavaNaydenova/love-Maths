6e4956231eeff1c8553f8c70ff8f7b0c95639f08
DesislavaNaydenova <deskata.01@gmail.com>
2024-03-06 18:55:28 +0000

    add code to calculate two random nubmers between 1 and 25

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
-
+/**
+ * The mail game Loop, called when the script is first loaded and 
+ * after the user's answer has been processed.
+ */
 function runGame() {
+    // Create 2 random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() *25) +1;
+    let num2 = Math.floor(Math.random() *25) +1;
 
 }
 
