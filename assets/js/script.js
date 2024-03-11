8f6f1f87ec880e17339dd65d5b2634fe6fe4edf2
DesislavaNaydenova <deskata.01@gmail.com>
2024-03-11 08:42:07 +0000

    add code to check the correct answera and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,28 +7,53 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
+
 });
+
 /**
- * The mail game Loop, called when the script is first loaded and 
- * after the user's answer has been processed.
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
  */
-function runGame() {
-    // Create 2 random numbers between 1 and 25
-    let num1 = Math.floor(Math.random() *25) +1;
-    let num2 = Math.floor(Math.random() *25) +1;
+function runGame(gameType) {
+
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
-
+/**
+ * Checks the answer against the first element in the
+ * returned calculateCorrectAnswer arrey
+ */
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    if (isCorrect) {
+        alert ("Hey you got it right!");
+}
+else{
+    alert ("Awww ... you answered ${userAnswer}. And the correct answer was ${calculatedAnswer[0]}");
+}
 
 }
+
 /**
  * Gets the operands (the numbers) and the operator (plus, minus etc)
  * directly from the dom, and returns the correct answer.
@@ -56,8 +81,12 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 }
 
 function displaySubtractQuestion() {
